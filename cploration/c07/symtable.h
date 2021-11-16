2dc94024ed70e9f55eacb67cf38e600b6666496c
Benjamin Foley <foleybe@oregonstate.edu>
2021-11-15 20:35:28 -0800

    completed the cploration 6

---
+++ cploration/c07/symtable.h
@@ -0,0 +1,17 @@
+#include<stdint.h>
+#include <stdio.h>   
+#include <stdlib.h> 
+#include<stdbool.h>
+#define SYMBOL_TABLE_SIZE  100
+typedef int16_t hack_addr;
+
+typedef struct {
+    hack_addr address;
+    char* name;
+}Symbol;
+
+Symbol* hashArray[SYMBOL_TABLE_SIZE];
+int hash(char *str);
+struct Symbol *find(char * key);
+void insert(char* key, hack_addr addr);
+void display_table();
