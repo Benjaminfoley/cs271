45f822f9f98e08076b1d080aaa50e2713378d6b1
Benjmain <73439535+Benjaminfoley@users.noreply.github.com>
2021-12-09 20:02:45 -0800

    main.c

--- c11 project 6/main.c
+++ c11 project 6/main.c
@@ -5,23 +5,24 @@
 #define MAX_INSTRUCTION_COUNT 30000
 
 main(int argc, const char *argv[]){
+  assemble(argv[1], instructions, num_instructions);
   if (argc != 2) {
     exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]);     
-    instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
+  
 
     printf("Usage: %s [filename]\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+  instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
   FILE *fin = fopen( argv[1], "r" );
-    if (fin == NULL){ 
-      exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
-    
-
+  if (fin == NULL){ 
+    exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
     perror("Unable to open file!");
     exit(EXIT_FAILURE);
   }
   int num_instructions = parse(fin, instructions);
   fclose(fin);
+
   free(instructions);
   return 0;
 }
